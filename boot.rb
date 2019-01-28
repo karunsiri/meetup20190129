41c260f89331c341349a61f5096faa149cc597a8
Karun S <karoon.siri@gmail.com>
2019-01-28 17:17:28 +0700

    Make use of debugging

--- boot.rb
+++ boot.rb
@@ -5,6 +5,16 @@ Dir['./lib/**/*.rb'].each do |file|
   require file
 end
 
+class Object
+  def self.after_call(name, &block)
+    original_method = instance_method(name)
+    define_method name do |*args|
+      original_method.bind(self).call(*args)
+      block.call
+    end
+  end
+end
+
 spanish = Spanish.new
 spanish.greet 'hello'
 spanish.greet 'u good?'
